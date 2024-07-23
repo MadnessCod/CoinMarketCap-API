34908de6a3b07a6715abb277830df81f30afdaf3
unknown <sadrahosseini98@gmail.com>
2024-07-23 20:03:05 +0330

    changed metaadta_database function, added request function to class, so far stable

--- Main.py
+++ Main.py
@@ -1,27 +1,38 @@
-import json
+import time
 
+import peewee
 import requests
+
 from datetime import datetime
 
 from celery import Celery
 from local_settings import API_KEY, BROKER_URL, BACKEND_URL
-from database_creation import (Map, Coin, Platform, URLs,
-                               ContractAddress, MetaData, Tags,
-                               MetadataTag, MetadataUrl, MetadataContractAddress)
+from database_creation import (
+    Map,
+    Coin,
+    Platform,
+    URLs,
+    ContractAddress,
+    MetaData,
+    Tags,
+    MetadataTag,
+    MetadataUrl,
+    MetadataContractAddress,
+)
 
-app = Celery('CoinMarketCapTasks', broker=BROKER_URL, backend=BACKEND_URL)
-base_url = 'https://pro-api.coinmarketcap.com'
+app = Celery("CoinMarketCapTasks", broker=BROKER_URL, backend=BACKEND_URL)
+base_url = "https://pro-api.coinmarketcap.com"
 
 headers = {
-    'Accepts': 'application/json',
-    'Accept-Encoding': 'gzip, deflate,',
-    'X-CMC_PRO_API_KEY': API_KEY,
+    "Accepts": "application/json",
+    "Accept-Encoding": "gzip, deflate,",
+    "X-CMC_PRO_API_KEY": API_KEY,
 }
 
 endpoints = {
-    1: '/v1/cryptocurrency/map',
-    2: '/v2/cryptocurrency/info',
-    3: '/v1/cryptocurrency/listings/latest',
+    1: "/v1/cryptocurrency/map",
+    2: "/v2/cryptocurrency/info",
+    3: "/v1/cryptocurrency/listings/latest",
     # 4: '/v1/cryptocurrency/listings/historical',
     # 5: '/v2/cryptocurrency/quotes/latest',
     # 6: '/v2/cryptocurrency/quotes/historical',
@@ -42,73 +53,67 @@ endpoints = {
 @app.task
 def convert_date(date_string):
     if date_string:
-        data_time_obj = datetime.fromisoformat(date_string.replace('Z', '+00:00'))
+        data_time_obj = datetime.fromisoformat(date_string.replace("Z", "+00:00"))
         return data_time_obj.date()
     return None
 
 
 @app.task
 def write_to_database(data):
-    for coin in data['data']:
-        coin_instance, _ = Coin.get_or_create(
-            cap_id=coin['id'],
-            name=coin['name'],
-            symbol=coin['symbol'],
-            slug=coin['slug'],
-        )
-        Map.get_or_create(
-            rank=int(coin['rank']),
-            is_active=bool(coin['is_active']),
-            first_date=convert_date.delay(coin['first_historical_data']).get(),
-            last_date=convert_date.delay(coin['last_historical_data']).get(),
-            coin=coin_instance,
-        )
-
-
-@app.task
-def relate(metadata, others, database_name):
-    for other in others:
-        database_name.get_or_create(
-            data=metadata,
-            other=other,
-        )
+    for coin in data["data"]:
+        try:
+            coin_instance, _ = Coin.get_or_create(
+                cap_id=coin["id"],
+                name=coin["name"],
+                symbol=coin["symbol"],
+                slug=coin["slug"],
+            )
+            Map.get_or_create(
+                rank=int(coin["rank"]),
+                is_active=bool(coin["is_active"]),
+                first_date=convert_date.delay(coin["first_historical_data"]).get(),
+                last_date=convert_date.delay(coin["last_historical_data"]).get(),
+                coin=coin_instance,
+            )
+        except KeyError as e:
+            print(f"Key Error: {e}")
 
 
 @app.task
 def metadata_database(data):
-    for coin in data['data'].values():
-        coin_instance = Coin.get(Coin.cap_id == coin['id'])
+    for coin in data["data"].values():
+        coin_instance = Coin.get(Coin.cap_id == coin["id"])
         contract_addresses = list()
-        for address in coin['contract_address']:
+        for address in coin["contract_address"]:
             inside_coin_instance, _ = Coin.get_or_create(
-                cap_id=address['platform']['coin']['id'],
-                name=address['platform']['coin']['name'],
-                symbol=address['platform']['coin']['symbol'],
-                slug=address['platform']['coin']['slug'],
+                cap_id=address["platform"]["coin"]["id"],
+                name=address["platform"]["coin"]["name"],
+                symbol=address["platform"]["coin"]["symbol"],
+                slug=address["platform"]["coin"]["slug"],
             )
             platform_instance, _ = Platform.get_or_create(
-                name=address['platform']['name'],
+                name=address["platform"]["name"],
                 coin=inside_coin_instance,
             )
             contract_instance, _ = ContractAddress.get_or_create(
-                contract_address=address['contract_address'],
+                contract_address=address["contract_address"],
                 platform=platform_instance,
             )
             contract_addresses.append(contract_instance)
 
-        tag_instances = tuple()
+        tag_instances = list()
         try:
-            for tag, category in zip(coin['tags'], coin['tag-groups']):
+            for tag, category in zip(coin["tags"], coin["tag-groups"]):
                 instance, _ = Tags.get_or_create(
                     name=tag,
                     category=category,
                 )
-                tag_instances += instance
+                tag_instances.append(instance)
         except TypeError:
-            print('there are no tags or groups')
+            print("there are no tags or groups")
 
         url_instance = list()
-        for name, links in coin['urls'].items():
+        for name, links in coin["urls"].items():
             for link in links:
                 instance, _ = URLs.get_or_create(
                     coin=coin_instance,
@@ -116,26 +121,46 @@ def metadata_database(data):
                     url=link,
                 )
                 url_instance.append(instance)
-        metadata_instance, _ = MetaData.get_or_create(
-            coin=coin_instance,
-            category=coin['category'],
-            description=coin['description'],
-            logo=coin['logo'],
-            subreddit=coin['subreddit'],
-            notice=coin['notice'],
-            platform=coin['platform'],  # this need fixing
-            twitter_username=coin['twitter_username'],
-            is_hidden=bool(coin['is_hidden']),
-            date_launched=convert_date(coin['date_launched']),
-            self_reported_circulating_supply=bool(coin['self_reported_circulating_supply']),
-            self_reported_tags=''.join(coin['self_reported_tags']) if coin['self_reported_tags'] else None,
-            self_reported_market_cap=coin['self_reported_market_cap'],
-            infinity_supply=coin['infinite_supply'],
-        )
-
-        relate.delay(metadata_instance, url_instance, MetadataUrl)
-        relate.delay(metadata_instance, tag_instances, MetadataTag)
-        relate.delay(metadata_instance, contract_addresses, MetadataContractAddress)
+        try:
+            metadata_instance, _ = MetaData.get_or_create(
+                coin=coin_instance,
+                category=coin["category"],
+                description=coin["description"],
+                logo=coin["logo"],
+                subreddit=coin["subreddit"],
+                notice=coin["notice"],
+                platform=coin["platform"],  # this need fixing
+                twitter_username=coin["twitter_username"],
+                is_hidden=bool(coin["is_hidden"]),
+                date_launched=convert_date(coin["date_launched"]),
+                self_reported_circulating_supply=bool(
+                    coin["self_reported_circulating_supply"]
+                ),
+                self_reported_tags=(
+                    "".join(coin["self_reported_tags"])
+                    if coin["self_reported_tags"]
+                    else None
+                ),
+                self_reported_market_cap=coin["self_reported_market_cap"],
+                infinity_supply=bool(coin["infinite_supply"]),
+            )
+        except peewee.DataError as e:
+            print(f"Error : {e}")
+        else:
+            for entry in url_instance:
+                MetadataUrl.get_or_create(
+                    data=metadata_instance,
+                    other=entry,
+                )
+            for entry in tag_instances:
+                MetadataTag.get_or_create(
+                    data=metadata_instance,
+                    other=entry,
+                )
+            for entry in contract_addresses:
+                MetadataContractAddress.get_or_create(
+                    data=metadata_instance, other=entry
+                )
 
 
 @app.task
@@ -143,15 +168,19 @@ def download(download_url):
     try:
         response = requests.get(download_url)
     except requests.exceptions.RequestException as e:
-        print(f'Requests Error {e}')
+        print(f"Requests Error {e}")
     else:
-        with open('image.jpg', 'wb') as f:
+        with open("image.jpg", "wb") as f:
             f.write(response.content)
+
+
+# @app.task
+# def latest()
 
 
 class CoinMarketCapApi:
     def __init__(self, endpoint):
-        self.url = 'https://pro-api.coinmarketcap.com'
+        self.url = "https://pro-api.coinmarketcap.com"
         self.headers = headers
         self.endpoint = endpoint
 
@@ -159,32 +188,34 @@ class CoinMarketCapApi:
         try:
             response = requests.get(url=url, headers=self.headers, params=parameters)
         except requests.exceptions.RequestException as error:
-            print(f'Requests Error {error}')
+            print(f"Requests Error {error}")
         else:
-            return response.json()
+            if response.status_code == 200:
+                return response
 
     def get(self):
-        map_url = f'{self.url}{self.endpoint}'
+        map_url = f"{self.url}{self.endpoint}"
         response = self.request(map_url)
         write_to_database.delay(response.json())
 
     def metadata_get(self):
         coins = Coin.select()
-        query = '?id='
+        query = "?id="
         for counter, coin in enumerate(coins):
-            query += f'{coin.cap_id},'
+            query += f"{coin.cap_id},"
             if counter % 100 == 0 and counter != 0:
-                metadata_url = f'{self.url}{self.endpoint}{query[:-1]}'
+                metadata_url = f"{self.url}{self.endpoint}{query[:-1]}"
+                time.sleep(2)
                 response = self.request(metadata_url)
-                query = '?id='
                 metadata_database.delay(response.json())
+                query = "?id="
 
     def latest(self):
         coins = Coin.select()
         for number in range(1, len(coins), 5000):
             parameters = {
-                'start': number,
-                'limit': 5000,
+                "start": number,
+                "limit": 5000,
             }
             latest_url = self.url + self.endpoint
             response = self.request(latest_url, parameters)
--- database_creation.py
+++ database_creation.py
@@ -4,11 +4,11 @@ from database_manager import DatabaseManager
 from local_settings import DATABASE
 
 database_manager = DatabaseManager(
-    database_name=DATABASE['name'],
-    user=DATABASE['user'],
-    password=DATABASE['password'],
-    host=DATABASE['host'],
-    port=DATABASE['port'],
+    database_name=DATABASE["name"],
+    user=DATABASE["user"],
+    password=DATABASE["password"],
+    host=DATABASE["host"],
+    port=DATABASE["port"],
 )
 
 
@@ -18,69 +18,71 @@ class MyBaseModel(peewee.Model):
 
 
 class Coin(MyBaseModel):
-    cap_id = peewee.CharField(unique=True, verbose_name='Coin id')
-    name = peewee.CharField(max_length=100, verbose_name='Coin name')
-    symbol = peewee.CharField(max_length=100, verbose_name='Coin symbol', null=True)
-    slug = peewee.CharField(max_length=100, verbose_name='Coin slug', null=True)
+    cap_id = peewee.IntegerField(unique=True, verbose_name="Coin id")
+    name = peewee.CharField(max_length=100, verbose_name="Coin name")
+    symbol = peewee.CharField(max_length=100, verbose_name="Coin symbol", null=True)
+    slug = peewee.CharField(max_length=100, verbose_name="Coin slug", null=True)
 
 
 class Platform(MyBaseModel):
     name = peewee.CharField()
     coin = peewee.ForeignKeyField(
         Coin,
-        on_delete='CASCADE',
-        verbose_name='coin',
+        on_delete="CASCADE",
+        verbose_name="coin",
     )
 
 
 class ContractAddress(MyBaseModel):
-    contract_address = peewee.CharField(verbose_name='Contract Address')
+    contract_address = peewee.CharField(verbose_name="Contract Address")
     platform = peewee.ForeignKeyField(
         Platform,
-        on_delete='CASCADE',
-        verbose_name='platform',
+        on_delete="CASCADE",
+        verbose_name="platform",
         null=True,
     )
 
 
 class Tags(MyBaseModel):
-    name = peewee.CharField(max_length=100, verbose_name='Name')
-    category = peewee.CharField(max_length=100, verbose_name='Category')
+    name = peewee.CharField(max_length=100, verbose_name="Name")
+    category = peewee.CharField(max_length=100, verbose_name="Category")
 
 
 class URLs(MyBaseModel):
     coin = peewee.ForeignKeyField(
         Coin,
-        on_delete='CASCADE',
-        verbose_name='coin',
+        on_delete="CASCADE",
+        verbose_name="coin",
     )
-    name = peewee.CharField(max_length=100, verbose_name='Name')
-    url = peewee.CharField(verbose_name='URL', null=True)
+    name = peewee.CharField(max_length=100, verbose_name="Name")
+    url = peewee.CharField(verbose_name="URL", null=True)
 
 
 class MetaData(MyBaseModel):
     coin = peewee.ForeignKeyField(
         Coin,
-        on_delete='CASCADE',
-        verbose_name='Coin',
+        on_delete="CASCADE",
+        verbose_name="Coin",
     )
-    category = peewee.CharField(verbose_name='Category')
-    description = peewee.TextField(verbose_name='Description')
-    logo = peewee.BlobField(verbose_name='Logo')
-    subreddit = peewee.CharField(verbose_name='Subreddit')
-    notice = peewee.CharField(verbose_name='Notice', null=True)
-    platform = peewee.CharField(verbose_name='Platform', null=True)
-    twitter_username = peewee.CharField(verbose_name='Twitter Username', null=True)
-    is_hidden = peewee.BooleanField(default=False, verbose_name='Hidden')
-    date_launched = peewee.DateField(verbose_name='Date Launched', null=True)
+    category = peewee.CharField(verbose_name="Category", null=True)
+    description = peewee.TextField(verbose_name="Description", null=True)
+    logo = peewee.BlobField(verbose_name="Logo", null=True)
+    subreddit = peewee.CharField(verbose_name="Subreddit", null=True)
+    notice = peewee.TextField(verbose_name="Notice", null=True)
+    platform = peewee.TextField(verbose_name="Platform", null=True)
+    twitter_username = peewee.CharField(verbose_name="Twitter Username", null=True)
+    is_hidden = peewee.BooleanField(default=False, verbose_name="Hidden", null=True)
+    date_launched = peewee.DateField(verbose_name="Date Launched", null=True)
     self_reported_circulating_supply = peewee.BooleanField(
-        default=False,
-        verbose_name='Self Reported Circulating Supply',
-        null=True
+        default=False, verbose_name="Self Reported Circulating Supply", null=True
     )
-    self_reported_tags = peewee.TextField(verbose_name='Self Reported Tags', null=True)
-    self_reported_market_cap = peewee.FloatField(verbose_name='Self Reported Market Cap', null=True)
-    infinity_supply = peewee.BooleanField(default=False, verbose_name='Infinity Supply', null=True)
+    self_reported_tags = peewee.TextField(verbose_name="Self Reported Tags", null=True)
+    self_reported_market_cap = peewee.FloatField(
+        verbose_name="Self Reported Market Cap", null=True
+    )
+    infinity_supply = peewee.BooleanField(
+        default=False, verbose_name="Infinity Supply", null=True
+    )
 
 
 class MetadataTag(MyBaseModel):
@@ -99,34 +101,39 @@ class MetadataContractAddress(MyBaseModel):
 
 
 class Map(MyBaseModel):
-    coin = peewee.ForeignKeyField(
-        Coin,
-        on_delete='CASCADE',
-        verbose_name='Coin'
-    )
-    rank = peewee.IntegerField(verbose_name='Coin rank', null=True)
-    price = peewee.FloatField(verbose_name='Price', null=True)
-    volume = peewee.CharField(verbose_name='Volume', null=True)
-    market_cap = peewee.CharField(verbose_name='Market Cap', null=True)
-    dominance = peewee.FloatField(verbose_name='Dominance', null=True)
-    max_supply = peewee.CharField(verbose_name='Max supply', null=True)
-    circulating_supply = peewee.CharField(verbose_name='Circulating supply', null=True)
-    total_supply = peewee.CharField(verbose_name='Total supply', null=True)
-    market_pairs = peewee.IntegerField(verbose_name='Market Pairs', null=True)
+    coin = peewee.ForeignKeyField(Coin, on_delete="CASCADE", verbose_name="Coin")
+    rank = peewee.IntegerField(verbose_name="Coin rank", null=True)
+    price = peewee.FloatField(verbose_name="Price", null=True)
+    volume = peewee.CharField(verbose_name="Volume", null=True)
+    market_cap = peewee.CharField(verbose_name="Market Cap", null=True)
+    dominance = peewee.FloatField(verbose_name="Dominance", null=True)
+    max_supply = peewee.CharField(verbose_name="Max supply", null=True)
+    circulating_supply = peewee.CharField(verbose_name="Circulating supply", null=True)
+    total_supply = peewee.CharField(verbose_name="Total supply", null=True)
+    market_pairs = peewee.IntegerField(verbose_name="Market Pairs", null=True)
     is_active = peewee.BooleanField(default=True, null=True)
-    first_date = peewee.DateField(verbose_name='Coin first date', null=True)
-    last_date = peewee.DateField(verbose_name='Coin last date', null=True)
+    first_date = peewee.DateField(verbose_name="Coin first date", null=True)
+    last_date = peewee.DateField(verbose_name="Coin last date", null=True)
 
 
-if __name__ == '__main__':
-    models = [Coin, Platform, ContractAddress, Tags, URLs,
-              MetaData, Map, MetadataContractAddress, MetadataTag, MetadataUrl
-              ]
+if __name__ == "__main__":
+    models = [
+        Coin,
+        Platform,
+        ContractAddress,
+        Tags,
+        URLs,
+        MetaData,
+        Map,
+        MetadataContractAddress,
+        MetadataTag,
+        MetadataUrl,
+    ]
     try:
         for model in models:
             if not model.table_exists():
                 database_manager.create_tables(models=[model])
     except peewee.DatabaseError as error:
-        print(f'Database Error : {error}')
+        print(f"Database Error : {error}")
     finally:
         database_manager.db.close()
