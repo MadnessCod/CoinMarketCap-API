a862feeec862d949ef3fa08577fccfc11763dd60
unknown <sadrahosseini98@gmail.com>
2024-07-29 21:21:17 +0330

    added tasks.py

--- Main.py
+++ Main.py
@@ -1,25 +1,9 @@
 import time
-
-from datetime import datetime
-
-import peewee
 import requests
 
-from celery import Celery
-from local_settings import API_KEY, BROKER_URL, BACKEND_URL
-from database_creation import (
-    Coin,
-    Platform,
-    URLs,
-    ContractAddress,
-    Tags,
-    CoinTag,
-    CoinUrl,
-    CoinContractAddress,
-)
-
-app = Celery("CoinMarketCapTasks", broker=BROKER_URL, backend=BACKEND_URL)
-app.broker_connection_retry_on_startup = True
+from tasks import metadata_database, write_to_database, latest_database
+from database_creation import Coin
+from local_settings import API_KEY
 
 base_url = "https://pro-api.coinmarketcap.com"
 
@@ -28,7 +12,6 @@ headers = {
     "Accept-Encoding": "gzip, deflate,",
     "X-CMC_PRO_API_KEY": API_KEY,
 }
-
 
 endpoints = {
     1: "/v1/cryptocurrency/map",
@@ -51,203 +34,55 @@ endpoints = {
 }
 
 
-@app.task
-def convert_date(date_string):
-    if date_string:
-        data_time_obj = datetime.fromisoformat(date_string.replace("Z", "+00:00"))
-        return data_time_obj.date()
-    return None
-
-
-@app.task
-def write_to_database(data):
-    for coin in data["data"]:
-        try:
-            coin_instance, _ = Coin.get_or_create(
-                cap_id=coin["id"],
-                name=coin["name"],
-                symbol=coin["symbol"],
-                slug=coin["slug"],
-                rank=int(coin["rank"]),
-                is_active=bool(coin["is_active"]),
-                first_date=convert_date.delay(coin["first_historical_data"]).get(),
-                last_date=convert_date.delay(coin["last_historical_data"]).get(),
-            )
-        except KeyError as e:
-            print(f"Key Error: {e}")
-        except peewee.IntegrityError:
-            continue
-
-
-@app.task
-def metadata_database(data):
-    for coin in data["data"].values():
-        coin_instance = Coin.get(Coin.cap_id == coin["id"])
-        contract_addresses = list()
-        for address in coin["contract_address"]:
-            try:
-                inside_coin_instance = Coin.get(
-                    Coin.cap_id == int(address["platform"]["coin"]["id"])
-                )
-            except (peewee.DoesNotExist, peewee.IntegrityError):
-                inside_coin_instance = Coin.create(
-                    cap_id=int(address["platform"]["coin"]["id"]),
-                    name=address["platform"]["coin"]["name"],
-                    symbol=address["platform"]["coin"]["symbol"],
-                    slug=address["platform"]["coin"]["slug"],
-                )
-
-            platform_instance, _ = Platform.get_or_create(
-                name=address["platform"]["name"],
-                coin=inside_coin_instance,
-            )
-
-            contract_instance, _ = ContractAddress.get_or_create(
-                contract_address=address["contract_address"],
-                platform=platform_instance,
-            )
-            contract_addresses.append(contract_instance)
-
-        tag_instances = list()
-        try:
-            for tag, category in zip(coin["tags"], coin["tag-groups"]):
-                instance, _ = Tags.get_or_create(
-                    name=tag,
-                    category=category,
-                )
-                tag_instances.append(instance)
-        except TypeError:
-            print("there are no tags or groups")
-
-        url_instance = list()
-        for name, links in coin["urls"].items():
-            for link in links:
-                instance, _ = URLs.get_or_create(
-                    coin=coin_instance,
-                    name=name,
-                    url=link,
-                )
-                url_instance.append(instance)
-        try:
-            Coin.update(
-                category=coin["category"],
-                description=coin["description"],
-                logo=coin["logo"],
-                subreddit=coin["subreddit"],
-                notice=coin["notice"],
-                platform=coin["platform"],
-                twitter_username=coin["twitter_username"],
-                is_hidden=bool(coin["is_hidden"]),
-                date_launched=convert_date(coin["date_launched"]),
-                self_reported_circulating_supply=bool(
-                    coin["self_reported_circulating_supply"]
-                ),
-                self_reported_tags=(
-                    "".join(coin["self_reported_tags"])
-                    if coin["self_reported_tags"]
-                    else None
-                ),
-                self_reported_market_cap=coin["self_reported_market_cap"],
-                infinity_supply=bool(coin["infinite_supply"]),
-            ).where(Coin.cap_id == coin["id"]).execute()
-        except peewee.DataError as e:
-            print(f"Error : {e}")
-        else:
-            for entry in url_instance:
-                CoinUrl.get_or_create(
-                    data=coin_instance,
-                    other=entry,
-                )
-            for entry in tag_instances:
-                CoinTag.get_or_create(
-                    data=coin_instance,
-                    other=entry,
-                )
-            for entry in contract_addresses:
-                CoinContractAddress.get_or_create(data=coin_instance, other=entry)
-
-
-@app.task
-def latest_database(coin):
+def request(url, parameters=None):
     try:
-        Coin.get(Coin.cap_id == coin["id"])
-    except peewee.DoesNotExist:
-        Coin.create(
-            cap_id=coin["id"],
-            name=coin["name"],
-            symbol=coin["symbol"],
-            slug=coin["slug"],
-        )
-    try:
-        Coin.update(
-            price=float(coin["quote"]["USD"]["price"]),
-            volume=coin["quote"]["USD"]["volume_24h"],
-            market_cap=coin["quote"]["USD"]["market_cap"],
-            dominance=float(coin["quote"]["USD"]["market_cap_dominance"]),
-            max_supply=coin["max_supply"],
-            circulating_supply=coin["circulating_supply"],
-            total_supply=coin["total_supply"],
-            market_pairs=int(coin["num_market_pairs"]),
-        ).where(Coin.cap_id == coin["id"]).execute()
-
-    except KeyError as e:
-        print(f'Key Error : {e}')
-
-
-@app.task
-def download(download_url):
-    try:
-        response = requests.get(download_url)
-    except requests.exceptions.RequestException as e:
-        print(f"Requests Error {e}")
+        response = requests.get(url=url, headers=headers, params=parameters)
+    except requests.exceptions.HTTPError as http_error:
+        print(f'HTTP Error {http_error}')
+    except requests.exceptions.RequestException as error:
+        print(f"Requests Error {error}")
     else:
-        with open("image.jpg", "wb") as f:
-            f.write(response.content)
-
-
-class CoinMarketCapApi:
-    def __init__(self, endpoint):
-        self.url = "https://pro-api.coinmarketcap.com"
-        self.headers = headers
-        self.endpoint = endpoint
-
-    def request(self, url, parameters=None):
-        try:
-            response = requests.get(url=url, headers=self.headers, params=parameters)
-        except requests.exceptions.HTTPError as http_error:
-            print(f'HTTP Error {http_error}')
-        except requests.exceptions.RequestException as error:
-            print(f"Requests Error {error}")
-        else:
-            if response.status_code == requests.codes.ok:
+        if response.status_code == requests.codes.ok:
+            if response is not None:
                 return response
 
-    def get(self):
-        map_url = f"{self.url}{self.endpoint}"
-        response = self.request(map_url)
-        write_to_database.delay(response.json())
 
-    def metadata_get(self):
-        coins = Coin.select()
-        query = "?id="
-        for counter, coin in enumerate(coins):
-            query += f"{coin.cap_id},"
-            if counter % 99 == 0 and counter != 0:
-                metadata_url = f"{self.url}{self.endpoint}{query[:-1]}"
-                time.sleep(2)
-                response = self.request(metadata_url)
-                metadata_database.delay(response.json())
-                query = "?id="
+def get():
+    map_url = f"{base_url}{endpoints[1]}"
+    response = request(map_url)
+    write_to_database.delay(response.json())
 
-    def latest(self):
-        coins = Coin.select()
-        for number in range(1, len(coins), 5000):
-            parameters = {
-                "start": number,
-                "limit": 5000,
-            }
-            latest_url = self.url + self.endpoint
-            response = self.request(latest_url, parameters=parameters)
-            response = response.json()
-            for coin in response['data']:
-                latest_database.delay(coin)
+
+def metadata_get():
+    coins = Coin.select()
+    query = "?id="
+    for counter, coin in enumerate(coins):
+        query += f"{coin.cap_id},"
+        if counter % 50 == 0 and counter != 0:
+            metadata_url = f"{base_url}{endpoints[2]}{query[:-1]}"
+            response = request(metadata_url)
+            time.sleep(1)
+            query = "?id="
+            if response:
+                response = response.json()
+                for count, entry in enumerate(response['data'].values()):
+                    metadata_database.delay(entry)
+                    time.sleep(2)
+
+
+def latest():
+    coins = Coin.select()
+    for number in range(1, len(coins), 5000):
+        parameters = {
+            "start": number,
+            "limit": 5000,
+        }
+        latest_url = f'{base_url}{endpoints[2]}'
+        response = request(latest_url, parameters=parameters)
+        response = response.json()
+        for coin in response['data']:
+            latest_database.delay(coin)
+
+
+if __name__ == '__main__':
+    metadata_get()
---
+++ tasks.py
@@ -0,0 +1,190 @@
+import time
+
+from datetime import datetime
+
+import peewee
+import requests
+
+from celery import Celery
+from local_settings import API_KEY, BROKER_URL, BACKEND_URL
+from database_creation import (
+    Coin,
+    Platform,
+    URLs,
+    ContractAddress,
+    Tags,
+    CoinTag,
+    CoinUrl,
+    CoinContractAddress,
+)
+
+app = Celery("CoinMarketCapTasks", broker=BROKER_URL, backend=BACKEND_URL)
+app.autodiscover_tasks(["tasks"])
+app.broker_connection_retry_on_startup = True
+
+
+@app.task
+def convert_date(date_string):
+    if date_string:
+        data_time_obj = datetime.fromisoformat(date_string.replace("Z", "+00:00"))
+        return data_time_obj.date()
+    return None
+
+
+@app.task(bind=True, max_retries=3, default_retry_delay=10)
+def request(url, parameters=None):
+    try:
+        response = requests.get(url=url, headers=headers, params=parameters)
+    except requests.exceptions.HTTPError as http_error:
+        print(f'HTTP Error {http_error}')
+    except requests.exceptions.RequestException as error:
+        print(f"Requests Error {error}")
+    else:
+        if response.status_code == requests.codes.ok:
+            if response is not None:
+                return response
+
+
+@app.task
+def write_to_database(data):
+    for coin in data["data"]:
+        try:
+            coin_instance, _ = Coin.get_or_create(
+                cap_id=coin["id"],
+                name=coin["name"],
+                symbol=coin["symbol"],
+                slug=coin["slug"],
+                rank=int(coin["rank"]),
+                is_active=bool(coin["is_active"]),
+                first_date=convert_date.delay(coin["first_historical_data"]).get(),
+                last_date=convert_date.delay(coin["last_historical_data"]).get(),
+            )
+        except KeyError as e:
+            print(f"Key Error: {e}")
+        except peewee.IntegrityError:
+            continue
+
+
+@app.task(bind=True, max_retries=5, default_retry_delay=60)
+def metadata_database(coin):
+    coin_instance = Coin.get(Coin.cap_id == coin["id"])
+    contract_addresses = list()
+    for address in coin["contract_address"]:
+        try:
+            inside_coin_instance = Coin.get(
+                Coin.cap_id == int(address["platform"]["coin"]["id"])
+            )
+        except (peewee.DoesNotExist, peewee.IntegrityError):
+            inside_coin_instance = Coin.create(
+                cap_id=int(address["platform"]["coin"]["id"]),
+                name=address["platform"]["coin"]["name"],
+                symbol=address["platform"]["coin"]["symbol"],
+                slug=address["platform"]["coin"]["slug"],
+            )
+
+        platform_instance, _ = Platform.get_or_create(
+            name=address["platform"]["name"],
+            coin=inside_coin_instance,
+        )
+
+        contract_instance, _ = ContractAddress.get_or_create(
+            contract_address=address["contract_address"],
+            platform=platform_instance,
+        )
+        contract_addresses.append(contract_instance)
+
+    tag_instances = list()
+    try:
+        for tag, category in zip(coin["tags"], coin["tag-groups"]):
+            instance, _ = Tags.get_or_create(
+                name=tag,
+                category=category,
+            )
+            tag_instances.append(instance)
+    except TypeError:
+        print("there are no tags or groups")
+
+    url_instance = list()
+    for name, links in coin["urls"].items():
+        for link in links:
+            instance, _ = URLs.get_or_create(
+                coin=coin_instance,
+                name=name,
+                url=link,
+            )
+            url_instance.append(instance)
+    try:
+        Coin.update(
+            category=coin["category"],
+            description=coin["description"],
+            logo=download.delay(coin["logo"]).get(),
+            subreddit=coin["subreddit"],
+            notice=coin["notice"],
+            platform=coin["platform"],
+            twitter_username=coin["twitter_username"],
+            is_hidden=bool(coin["is_hidden"]),
+            date_launched=convert_date(coin["date_launched"]),
+            self_reported_circulating_supply=bool(
+                coin["self_reported_circulating_supply"]
+            ),
+            self_reported_tags=(
+                "".join(coin["self_reported_tags"])
+                if coin["self_reported_tags"]
+                else None
+            ),
+            self_reported_market_cap=coin["self_reported_market_cap"],
+            infinity_supply=bool(coin["infinite_supply"]),
+        ).where(Coin.cap_id == coin["id"]).execute()
+    except peewee.DataError as e:
+        print(f"Error : {e}")
+    else:
+        for entry in url_instance:
+            CoinUrl.get_or_create(
+                data=coin_instance,
+                other=entry,
+            )
+        for entry in tag_instances:
+            CoinTag.get_or_create(
+                data=coin_instance,
+                other=entry,
+            )
+        for entry in contract_addresses:
+            CoinContractAddress.get_or_create(data=coin_instance, other=entry)
+
+
+@app.task
+def latest_database(coin):
+    try:
+        Coin.get(Coin.cap_id == coin["id"])
+    except peewee.DoesNotExist:
+        Coin.create(
+            cap_id=coin["id"],
+            name=coin["name"],
+            symbol=coin["symbol"],
+            slug=coin["slug"],
+        )
+    try:
+        Coin.update(
+            price=float(coin["quote"]["USD"]["price"]),
+            volume=coin["quote"]["USD"]["volume_24h"],
+            market_cap=coin["quote"]["USD"]["market_cap"],
+            dominance=float(coin["quote"]["USD"]["market_cap_dominance"]),
+            max_supply=coin["max_supply"],
+            circulating_supply=coin["circulating_supply"],
+            total_supply=coin["total_supply"],
+            market_pairs=int(coin["num_market_pairs"]),
+        ).where(Coin.cap_id == coin["id"]).execute()
+
+    except KeyError as e:
+        print(f'Key Error : {e}')
+
+
+@app.task
+def download(download_url):
+    try:
+        response = requests.get(download_url)
+    except requests.exceptions.RequestException as e:
+        print(f"Requests Error {e}")
+        return None
+    else:
+        return response.content
